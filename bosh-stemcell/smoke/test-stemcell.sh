58e01d3054c56fdf96954ccacf6512ed3a6c9641
Saman Alvi <salvi@pivotal.io>
2016-11-10 11:22:29 -0500

    Modified properties
    Signed-off-by: Andrew Su <asu@pivotal.io>

--- bosh-stemcell/smoke/test-stemcell.sh
+++ bosh-stemcell/smoke/test-stemcell.sh
@@ -129,6 +129,7 @@ instance_groups:
   - name: syslog_forwarder
     release: syslog
     properties:
+      syslog:
         forward_files: true
     consumes:
       syslog_storer: { from: syslog_storer }
@@ -184,7 +185,5 @@ LOGFILE=/var/vcap/sys/log/deep/path/deepfile.log
 
 bosh -d ./deployment.yml ssh syslog_forwarder 0 "echo c1oudc0w | sudo -S mkdir -p /var/vcap/sys/log/deep/path && sudo touch ${LOGFILE} && echo 'test-deep-blackbox-msg' | sudo tee -a ${LOGFILE}"
 bosh -d ./deployment.yml scp --download syslog_storer 0 /var/vcap/store/syslog_storer/syslog.log ${DOWNLOAD_DESTINATION}
-
-cat ${DOWNLOAD_DESTINATION}/syslog.*
 
 grep 'test-deep-blackbox-msg' ${DOWNLOAD_DESTINATION}/syslog.* || ( echo "was not able to get message forwarded from BlackBox" ; exit 1 )
