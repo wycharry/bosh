2b80dd4a5d16519027830ea63fc46f7ff4e250d7
Brian Cunnie <cunnie@pivotal.io>
2017-01-06 19:59:56 -0800

    add ipv6 support manual networking
    - switches IpAddress model to store ip addresses as strings
    - renamed column to address_str to avoid confusion that address returns int string
    
    Signed-off-by: dmitriy kalinin <dkalinin@pivotal.io>

--- src/bosh-director/spec/unit/deployment_plan/instance_repository_spec.rb
+++ src/bosh-director/spec/unit/deployment_plan/instance_repository_spec.rb
@@ -61,7 +61,7 @@ describe Bosh::Director::DeploymentPlan::InstanceRepository do
     describe 'binding existing reservations' do
       context 'when instance has reservations in db' do
         before do
-          existing_instance.add_ip_address(BD::Models::IpAddress.make(address: 123))
+          existing_instance.add_ip_address(BD::Models::IpAddress.make(address_str: "123"))
         end
 
         it 'is using reservation from database' do
@@ -161,7 +161,7 @@ describe Bosh::Director::DeploymentPlan::InstanceRepository do
     describe 'binding existing reservations' do
       context 'when instance has reservations in db' do
         before do
-          existing_instance.add_ip_address(BD::Models::IpAddress.make(address: 123))
+          existing_instance.add_ip_address(BD::Models::IpAddress.make(address_str: "123"))
         end
 
         it 'is using reservation from database' do
